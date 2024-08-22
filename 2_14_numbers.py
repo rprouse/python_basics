05537a1dc529c1533869ea24cedee8baa19fb126
Rob Prouse <rob@prouse.org>
2024-08-21 21:28:01 -0400

    Add Python numbers example file
    This commit adds a new file, 2_14_numbers.py, which demonstrates various number operations in Python. It includes examples of exponentiation, binary shifting, adding numbers to strings, and performing arithmetic operations with different types of numbers.

---
+++ 2_14_numbers.py
@@ -0,0 +1,17 @@
+n1 = 5
+n2 = 3
+
+# n1 to the power of n2
+print(n1 ** n2)
+
+# 0001 in binary
+n3 = 0x1
+
+# Shift left by 3 bits, which is 1000 in binary or 8 in decimal
+print(n3 << 3)
+
+# Add a number to a string
+print('Number: ' + str(n1))
+
+# Numbers hold small numbers, big numbers, and complex numbers, etc.
+print(5 / 3)
